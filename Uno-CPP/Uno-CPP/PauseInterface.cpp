fab9c372a8b559bd6778ba80d03b3b0931efcfea
Squirrelbear <petermitchell0001@gmail.com>
2022-01-13 12:19:21 +1030

    Added text and buttons to PauseInterface.

--- Uno-CPP/Uno-CPP/PauseInterface.cpp
+++ Uno-CPP/Uno-CPP/PauseInterface.cpp
@@ -14,6 +14,9 @@ PauseInterface::~PauseInterface()
 void PauseInterface::draw(sf::RenderWindow & renderWindow) const
 {
 	_interfaceBackground->draw(renderWindow);
+	for (const auto& button : _buttonList) {
+		button.draw(renderWindow);
+	}
 }
 
 void PauseInterface::initialiseInterface(const sf::IntRect & bounds, const sf::IntRect& gameBounds, const sf::Font & font)
@@ -27,7 +30,7 @@ void PauseInterface::initialiseInterface(const sf::IntRect & bounds, const sf::I
 	DrawableShape* leftBackground = new DrawableShape(new sf::RectangleShape(sf::Vector2f(160 - 2, 90 - 2)), sf::Color(165, 177, 94, 205), sf::Vector2f(170 + 1, 300 + 1));
 	_interfaceBackground->addChild(leftBackground);
 	leftBackground->setBorder(1, sf::Color::Black);
-	DrawableShape* rightBackground = new DrawableShape(new sf::RectangleShape(sf::Vector2f(410 - 2, 300 - 2)), sf::Color(165, 177, 94, 205), sf::Vector2f(790+1, 220+1));
+	DrawableShape* rightBackground = new DrawableShape(new sf::RectangleShape(sf::Vector2f(420 - 2, 270 - 2)), sf::Color(165, 177, 94, 205), sf::Vector2f(790+1, 220+1));
 	_interfaceBackground->addChild(rightBackground);
 	rightBackground->setBorder(1, sf::Color::Black);
 
@@ -35,6 +38,45 @@ void PauseInterface::initialiseInterface(const sf::IntRect & bounds, const sf::I
 	titleText->setOffset(sf::Vector2f(bounds.left + bounds.width / 2 - titleText->getTextWidth() / 2, bounds.top));
 	_interfaceBackground->addChild(titleText);
 
+	// LEFT SIZE TEXT
+	DrawableText* leftTextTitle = new DrawableText(sf::Vector2f(0, 0), "Controls", font, 20, sf::Color::Black, sf::Text::Bold);
+	leftTextTitle->setOffset(sf::Vector2f(210, 300));
+	_interfaceBackground->addChild(leftTextTitle);
 
+	std::vector<std::string> leftTextArray;
+	leftTextArray.push_back("Escape: Pause");
+	leftTextArray.push_back("Q: Sort hand");
+
+	for (int y = 0; y < leftTextArray.size(); y++) {
+		DrawableText* leftText = new DrawableText(sf::Vector2f(0, 0), leftTextArray.at(y), font, 20, sf::Color::Black, sf::Text::Bold);
+		leftText->setOffset(sf::Vector2f(180, 325+y*30));
+		_interfaceBackground->addChild(leftText);
+	}
+
+	// RIGHT SIDE TEXT
+	DrawableText* rightTextTitle = new DrawableText(sf::Vector2f(0, 0), "Debug Controls (0 first)", font, 20, sf::Color::Black, sf::Text::Bold);
+	rightTextTitle->setOffset(sf::Vector2f(880, 230));
+	_interfaceBackground->addChild(rightTextTitle);
+
+	std::vector<std::string> rightTextArray;
+	rightTextArray.push_back("0: Turn on Debug");
+	rightTextArray.push_back("9: Reveal All Hands");
+	rightTextArray.push_back("8: Toggle Turn Direction");
+	rightTextArray.push_back("7: Empty Player Hand");
+	rightTextArray.push_back("6: Remove Left Player Card");
+	rightTextArray.push_back("5: Toggle Show Turn Action Sequence");
+	rightTextArray.push_back("4: Toggle Show Turn Action Tree On Set");
+
+	for (int y = 0; y < rightTextArray.size(); y++) {
+		DrawableText* rightText = new DrawableText(sf::Vector2f(0, 0), rightTextArray.at(y), font, 20, sf::Color::Black, sf::Text::Bold);
+		rightText->setOffset(sf::Vector2f(800, 270 + y * 30));
+		_interfaceBackground->addChild(rightText);
+	}
+
+	_buttonList.emplace_back(Button(sf::IntRect(bounds.left + 6, bounds.top + 6 + 60, bounds.width - 12, 30), "Resume", 1, font));
+	_buttonList.emplace_back(Button(sf::IntRect(bounds.left + 6, bounds.top + 6 + 30+6+60, bounds.width - 12, 30), "Return to Lobby", 3, font));
+	_buttonList.emplace_back(Button(sf::IntRect(bounds.left + 6, bounds.top + 6 + (30+6)*2 + 60, bounds.width - 12, 30), "Resume", 2, font));
+
+	// Force all components to update to their correct positions.
 	_interfaceBackground->setPositionWithOffset(sf::Vector2f(gameBounds.left, gameBounds.top));
 }
--- Uno-CPP/Uno-CPP/PauseInterface.h
+++ Uno-CPP/Uno-CPP/PauseInterface.h
@@ -3,19 +3,40 @@
 #include "DrawableObjectGroup.h"
 #include "Button.h"
 
+/**
+ * Uno
+ *
+ * PauseInterface class:
+ * A simple pause interface that allows a couple of buttons to control
+ * game flow or otherwise just pause for a moment.
+ *
+ * @author Peter Mitchell
+ * @version 2022.1
+ */
 class PauseInterface :
 	public WndInterface
 {
 public:
+	/**
+	 * Initialise the interface with bounds makes it ready to use.
+	 *
+	 * @param bounds The region to draw this interface in.
+	 * @param gameBounds The bounds of the entire application to apply an transparent overlay.
+	 * @param font Reference to the font for initialising text elements.
+	 */
 	PauseInterface(const sf::IntRect& bounds, const sf::IntRect& gameBounds, const sf::Font& font);
 	virtual ~PauseInterface();
 
 	// Do nothing during update.
 	virtual void update(const float deltaTime) override {}
+	// Draws the background elements and buttons on top.
 	virtual void draw(sf::RenderWindow & renderWindow) const override;
 
 private:
+	// The static background elements of the interface initialised by initialiseInterface().
 	DrawableObjectGroup* _interfaceBackground;
+	// A list of all the buttons in the interface.
+	std::vector<Button> _buttonList;
 
 	void initialiseInterface(const sf::IntRect & bounds, const sf::IntRect& gameBounds, const sf::Font & font);
 };
